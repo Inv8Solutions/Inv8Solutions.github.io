e44f7c0e52956823d8ca9c1924036bb6735931ae
nervachan <nervagrf@gmail.com>
2025-12-04 10:35:55 +0800

    fixed font face

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        'general-sans': ['GeneralSans', 'system-ui', 'sans-serif'],
-      },
+    fontFamily: {
+      sans: ['GeneralSans', 'system-ui', 'sans-serif'],
+      body: ['GeneralSans', 'system-ui', 'sans-serif'],
     },
+    extend: {},
   },
+  plugins: [],
 } satisfies Config
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
@@ -13,11 +13,41 @@ export default defineConfig({
     },
   },
   build: {
-    copyPublicDir: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name?.split('.').pop()
+          let extType = info?.toLowerCase()
+
+          if (!extType) return 'assets/[name]-[hash][extname]'
+
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images'
+          } else if (/woff|woff2|eot|ttf|otf/i.test(extType)) {
+            extType = 'fonts'
+          }
+
+          return `assets/${extType}/[name]-[hash][extname]`
+        },
+      },
+    },
   },
   server: {
     fs: {
       allow: ['..'],
+    },
+  },
+  publicDir: 'public',
+  assetsInclude: ['**/*.otf', '**/*.woff', '**/*.woff2'],
+  optimizeDeps: {
+    include: ['@/assets/fonts/*.woff2', '@/assets/fonts/*.woff', '@/assets/fonts/*.otf'],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/assets/main.css';`,
+      },
     },
   },
 })
