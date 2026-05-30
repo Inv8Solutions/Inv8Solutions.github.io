24745906125e8ed7723dafaba39c77d89d72286a
leandroreyw <leandrogepila@gmail.com>
2026-05-30 18:15:41 +0800

    Fix broken images: convert relative src paths to JS imports in Marquee components

--- src/components/Home/MarqueeBottom.vue
+++ src/components/Home/MarqueeBottom.vue
@@ -2,24 +2,19 @@
   <vue3-marquee direction="reverse" :pauseOnHover="false" :clone="false" :duration="30">
     <div class="flex items-center gap-10 last:mr-10 my-10">
       <div class="rounded-lg shadow-lg">
-        <img src="./../../assets/introImage6.png" class="h-96 w-auto rounded-lg object-cover" />
+        <img :src="img6" class="h-96 w-auto rounded-lg object-cover" />
       </div>
-
       <div class="rounded-lg shadow-lg">
-        <img src="./../../assets/introImage7.png" class="h-96 w-auto rounded-lg object-cover" />
+        <img :src="img7" class="h-96 w-auto rounded-lg object-cover" />
       </div>
-
-      <!-- narrow container for phone screenshots -->
       <div class="rounded-lg shadow-lg w-48">
-        <img src="./../../assets/introImage8.png" class="h-96 w-full rounded-lg object-cover" />
+        <img :src="img8" class="h-96 w-full rounded-lg object-cover" />
       </div>
-
       <div class="rounded-lg shadow-lg">
-        <img src="./../../assets/introImage9.png" class="h-96 w-full rounded-lg object-cover" />
+        <img :src="img9" class="h-96 w-full rounded-lg object-cover" />
       </div>
-
       <div class="rounded-lg shadow-lg">
-        <img src="./../../assets/introImage10.png" class="h-96 w-auto rounded-lg object-cover" />
+        <img :src="img10" class="h-96 w-auto rounded-lg object-cover" />
       </div>
     </div>
   </vue3-marquee>
@@ -27,4 +22,9 @@
 
 <script setup lang="ts">
 import { Vue3Marquee } from 'vue3-marquee'
+import img6 from '@/assets/introImage6.png'
+import img7 from '@/assets/introImage7.png'
+import img8 from '@/assets/introImage8.png'
+import img9 from '@/assets/introImage9.png'
+import img10 from '@/assets/introImage10.png'
 </script>
--- src/components/Home/MarqueeTop.vue
+++ src/components/Home/MarqueeTop.vue
@@ -2,24 +2,19 @@
   <vue3-marquee direction="normal" :pauseOnHover="false" :clone="false" :duration="30">
     <div class="flex items-center gap-10 last:mr-10">
       <div class="rounded-lg shadow-lg">
-        <img src="./../../assets/introImage1.png" class="h-96 w-auto rounded-lg object-cover" />
+        <img :src="img1" class="h-96 w-auto rounded-lg object-cover" />
       </div>
-
       <div class="rounded-lg shadow-lg">
-        <img src="./../../assets/introImage2.png" class="h-96 w-auto rounded-lg object-cover" />
+        <img :src="img2" class="h-96 w-auto rounded-lg object-cover" />
       </div>
-
-      <!-- narrow container for phone screenshots -->
       <div class="rounded-lg shadow-lg w-48">
-        <img src="./../../assets/introImage3.png" class="h-96 w-full rounded-lg object-cover" />
+        <img :src="img3" class="h-96 w-full rounded-lg object-cover" />
       </div>
-
       <div class="rounded-lg shadow-lg w-48">
-        <img src="./../../assets/introImage4.png" class="h-96 w-full rounded-lg object-cover" />
+        <img :src="img4" class="h-96 w-full rounded-lg object-cover" />
       </div>
-
       <div class="rounded-lg shadow-lg">
-        <img src="./../../assets/introImage5.png" class="h-96 w-auto rounded-lg object-cover" />
+        <img :src="img5" class="h-96 w-auto rounded-lg object-cover" />
       </div>
     </div>
   </vue3-marquee>
@@ -27,4 +22,9 @@
 
 <script setup lang="ts">
 import { Vue3Marquee } from 'vue3-marquee'
+import img1 from '@/assets/introImage1.png'
+import img2 from '@/assets/introImage2.png'
+import img3 from '@/assets/introImage3.png'
+import img4 from '@/assets/introImage4.png'
+import img5 from '@/assets/introImage5.png'
 </script>
